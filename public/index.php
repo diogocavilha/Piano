bf99cdbe1de56ae364bcdcba8cf004abc29a7500
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2016-09-02 00:58:28 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once '../vendor/autoload.php';
-require_once 'routes.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/routes.php';
 
 if (getenv('APPLICATION_ENV') == 'development') {
     ini_set('display_errors', 1);
@@ -14,14 +14,14 @@ $layoutPerModule = [
     ]
 ];
 
-$config = new \Piano\Config();
-$config->setApplicationFolder('app')
-    ->setDefaultModule('application')
-    ->setLayoutPerModule($layoutPerModule);
+$config = new \Piano\Config\Ini(
+    __DIR__ . '/../src/app/config/config.ini'
+);
 
 $router = new \Piano\Router();
 $router->setRoutes($routes)
     ->enableSearchEngineFriendly(true);
 
 $app = new \Piano\Application($config, $router);
+$app->registerModulesLayout($layoutPerModule);
 $app->run();
