23b782e4a709e23fe6a246af217684347cab50e6
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2017-02-09 23:28:17 -0200

    Add controller unit test

--- src/Piano/Mvc/Controller.php
+++ src/Piano/Mvc/Controller.php
@@ -2,18 +2,11 @@
 
 namespace Piano\Mvc;
 
-use Piano\Mvc\View;
-
 /**
  * @author Diogo Alexsander Cavilha <diogocavilha@gmail.com>
  */
 class Controller
 {
-    /**
-     * @var Piano\Config
-     */
-    private $config;
-
     /**
      * @var Piano\Application
      */
@@ -26,44 +19,29 @@ class Controller
 
     public function __construct(\Piano\Application $application)
     {
-        $this->config = $application->getConfig();
         $this->application = $application;
-        $this->view = new View($application);
+        $this->view = new \Piano\Mvc\View($application);
 
         if (method_exists($this, 'initialize')) {
             $this->initialize();
         }
     }
 
-    public function getParams()
-    {
-        return $this->application->getParams();
-    }
-
-    public function getParam($name = null, $default = null)
-    {
-        if (is_null($name)) {
-            throw new \InvalidArgumentException('Param name is expected.');
-        }
-
-        return $this->application->getParam($name, $default);
-    }
-
     /**
      * @codeCoverageIgnore
      */
-    protected function redirect($url = null, array $args = null)
+    protected function redirect(string $url = null, array $args = null)
     {
         if (is_null($url)) {
             throw new \InvalidArgumentException('Param url is expected.');
         }
 
         $this->application->redirect($url, $args);
-        exit();
+        return;
     }
 
-    public function getApplication()
+    public function getDi() : \Piano\Container
     {
-        return $this->application;
+        return $this->application->getDi();
     }
 }
--- tests/Piano/Mvc/ControllerTest.php
+++ tests/Piano/Mvc/ControllerTest.php
@@ -1,5 +1,12 @@
 <?php
 
+use Piano\Mvc\View;
+use Piano\Mvc\Controller;
+use Piano\Application;
+
+/**
+ * @group php7
+ */
 class ControllerTest extends PHPUnit_Framework_TestCase
 {
     private $controller;
@@ -8,89 +15,76 @@ class ControllerTest extends PHPUnit_Framework_TestCase
     {
         $_SERVER['REQUEST_URI'] = '/';
 
-        $config = $this->getConfig();
-        $router = $this->getRouter();
-        $router->enableSearchEngineFriendly();
+        $di = $this->getTestingContainer($sef = true);
+        $app = new Application($di);
 
-        $app = new Piano\Application($config, $router);
-
-        $this->controller = new Piano\Mvc\Controller($app);
+        $this->controller = new Controller($app);
     }
 
-    public function testGetParamsShouldWork()
+    public function testItCanReturnTheDiContainer()
     {
-        $this->assertEmpty($this->controller->getParams());
+        $this->assertTrue(
+            method_exists($this->controller, 'getDi'),
+            'Method "getDi()" must exist'
+        );
+
+        $this->assertInstanceOf(
+            'Piano\Container',
+            $this->controller->getDi()
+        );
     }
 
-    /**
-     * @expectedException InvalidArgumentException
-     * @expectedExceptionMessage Param name is expected.
-     */
-    public function testGetParamShouldThrowAnInvalidArgumentException()
+    private function getTestingContainer($searchEngineFriendly = true)
     {
-        $this->controller->getParam();
-    }
+        $container = new Piano\Container();
+        $container['config'] = function () {
+            return new Piano\Config\Ini('tests/configTest.ini');
+        };
 
-    /**
-     * @expectedException Exception
-     */
-    public function testGetParamShouldWork()
-    {
-        $this->controller->getParam('teste');
-    }
-
-    public function testGetApplicationShouldWork()
-    {
-        $this->assertInstanceOf('Piano\Application', $this->controller->getApplication());
-    }
-
-    private function getRouter()
-    {
-        $routes = [
-            'default' => [
-                'route' => '/',
-                'module' => 'application',
-                'controller' => 'index',
-                'action' => 'index'
-            ],
-            'defaultAdmin' => [
-                'route' => '/admin',
-                'module' => 'admin',
-                'controller' => 'index',
-                'action' => 'index'
-            ],
-            'user_edit' => [
-                'route' => '/users/:id',
-                'module' => 'application',
-                'controller' => 'user',
-                'action' => 'edit',
-                [
-                    ':id' => '\d+'
+        $container['router'] = function () use ($searchEngineFriendly) {
+            $routes = [
+                'default' => [
+                    'route' => '/',
+                    'module' => 'application',
+                    'controller' => 'index',
+                    'action' => 'index'
+                ],
+                'defaultAdmin' => [
+                    'route' => '/admin',
+                    'module' => 'admin',
+                    'controller' => 'index',
+                    'action' => 'index'
+                ],
+                'userEdit' => [
+                    'route' => '/users/:id',
+                    'module' => 'application',
+                    'controller' => 'user',
+                    'action' => 'edit',
+                    [
+                        ':id' => '\d+'
+                    ]
+                ],
+                'error404' => [
+                    'route' => '/error',
+                    'module' => 'application',
+                    'controller' => 'error',
+                    'action' => 'error',
+                ],
+                'redirect' => [
+                    'route' => '/redirect/contact',
+                    'module' => 'application',
+                    'controller' => 'index',
+                    'action' => 'redirectTest',
                 ]
-            ],
-            'error_404' => [
-                'route' => '/error',
-                'module' => 'application',
-                'controller' => 'error',
-                'action' => 'error',
-            ],
-            'redirect' => [
-                'route' => '/redirect/contact',
-                'module' => 'application',
-                'controller' => 'index',
-                'action' => 'redirectTest',
-            ]
-        ];
+            ];
 
-        $router = new Piano\Router();
-        $router->setRoutes($routes);
+            $router = new Piano\Router();
+            $router->setRoutes($routes);
+            $router->enableSearchEngineFriendly($searchEngineFriendly);
 
-        return $router;
-    }
+            return $router;
+        };
 
-    private function getConfig()
-    {
-        $config = new \Piano\Config\Ini('tests/configTest.ini');
-        return $config;
+        return $container;
     }
 }
